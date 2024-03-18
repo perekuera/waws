f4cd58c240247204465d5b700f06f6e62fbfa79b
Pere Bestard <perekuera@gmail.com>
2024-03-18 09:43:05 +0000

    progress

--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 )
 
@@ -14,9 +15,23 @@ func main() {
 
     command := strings.ToLower(os.Args[1])
 
+    var subcommand string
+
     switch command {
     case "codecommit", "cc", "-c":
-        fmt.Println("Not implemented yet!")
+        subcommand = strings.ToLower(os.Args[2]);
+        switch subcommand {
+        case "list", "l", "-l":
+            var query string = "repositories[]"
+            if len(os.Args) > 3 {
+                query = fmt.Sprintf("repositories[?contains(repositoryName, '%s')]", os.Args[3])
+            }
+            listRepositories(query)
+        case "clone", "c", "-c":
+        default:
+            fmt.Println("Invalid subcommand:", subcommand)
+            os.Exit(1)
+        }
         os.Exit(0)
     case "codebuild", "cb", "-b":
         fmt.Println("Not implemented yet!")
@@ -24,7 +39,7 @@ func main() {
     case "help", "h":
         showHelp()
     default:
-        fmt.Println("Invalid option:", command)
+        fmt.Println("Invalid command:", command)
         os.Exit(1)
     }
 
@@ -37,11 +52,21 @@ func showHelp() {
 Commands/subcommands:
     codecommit              cc  -c      CodeCommit operations
     
-        list [search]       l   -l      List repositories
+        list [QUERY]       l   -l      List repositories
         clone REPO_NAME     c   -c      Clone repository
 
   codebuild                 db  -b      CodeBuild operations
 
   help                      h   -h      This help
 `)
+}
+
+func listRepositories(query string) {
+    cmd := exec.Command("aws", "codecommit", "list-repositories", "--output", "text", "--query", query)
+    output, err := cmd.CombinedOutput()
+    if err != nil {
+        fmt.Println("Error listing repositories:", err)
+        os.Exit(1)
+    }
+    fmt.Println(string(output))
 }
